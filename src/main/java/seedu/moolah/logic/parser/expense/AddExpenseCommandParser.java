cbb7e1c884f8915b8acd30dbd45d2894949c9a5f
Ryo Armanda <ryoarmanda@rocketmail.com>
2019-11-06 15:45:05 +0800

    Add canteen menu support on add expenses

--- src/main/java/seedu/moolah/logic/parser/expense/AddExpenseCommandParser.java
+++ src/main/java/seedu/moolah/logic/parser/expense/AddExpenseCommandParser.java
@@ -5,6 +5,7 @@ import static seedu.moolah.commons.core.Messages.MESSAGE_REPEATED_PREFIX_COMMAND
 import static seedu.moolah.logic.commands.expense.AddExpenseCommand.MESSAGE_USAGE;
 import static seedu.moolah.logic.parser.CliSyntax.PREFIX_CATEGORY;
 import static seedu.moolah.logic.parser.CliSyntax.PREFIX_DESCRIPTION;
+import static seedu.moolah.logic.parser.CliSyntax.PREFIX_MENU;
 import static seedu.moolah.logic.parser.CliSyntax.PREFIX_PRICE;
 import static seedu.moolah.logic.parser.CliSyntax.PREFIX_TIMESTAMP;
 
@@ -26,7 +27,7 @@ import seedu.moolah.model.expense.Expense;
 import seedu.moolah.model.expense.Price;
 import seedu.moolah.model.expense.Timestamp;
 import seedu.moolah.model.expense.util.UniqueIdentifierGenerator;
-
+import seedu.moolah.model.menu.MenuItem;
 
 /**
  * Parses input arguments and creates a new AddExpenseCommand object
@@ -34,7 +35,7 @@ import seedu.moolah.model.expense.util.UniqueIdentifierGenerator;
 public class AddExpenseCommandParser implements Parser<AddExpenseCommand> {
 
     public static final List<Prefix> REQUIRED_PREFIXES = Collections.unmodifiableList(List.of(
-            PREFIX_DESCRIPTION, PREFIX_PRICE, PREFIX_CATEGORY
+            PREFIX_DESCRIPTION, PREFIX_PRICE, PREFIX_CATEGORY, PREFIX_MENU
     ));
 
     public static final List<Prefix> OPTIONAL_PREFIXES = Collections.unmodifiableList(List.of(
@@ -48,15 +49,49 @@ public class AddExpenseCommandParser implements Parser<AddExpenseCommand> {
      */
     public AddExpenseCommand parse(String args) throws ParseException {
         ArgumentMultimap argMultimap =
-                ArgumentTokenizer.tokenize(args, PREFIX_DESCRIPTION, PREFIX_PRICE, PREFIX_CATEGORY, PREFIX_TIMESTAMP);
+                ArgumentTokenizer.tokenize(args, PREFIX_DESCRIPTION, PREFIX_PRICE, PREFIX_CATEGORY, PREFIX_MENU,
+                        PREFIX_TIMESTAMP);
 
-        if (!arePrefixesPresent(argMultimap, PREFIX_DESCRIPTION, PREFIX_PRICE, PREFIX_CATEGORY)
-                || !argMultimap.getPreamble().isEmpty()) {
-            throw new ParseException(
-                    String.format(MESSAGE_INVALID_COMMAND_FORMAT,
-                    MESSAGE_USAGE));
+        boolean hasMenuItemFormat = arePrefixesPresent(argMultimap, PREFIX_MENU)
+                && argMultimap.getPreamble().isEmpty();
+        boolean hasUserInputFormat = arePrefixesPresent(argMultimap, PREFIX_DESCRIPTION, PREFIX_PRICE, PREFIX_CATEGORY)
+                && argMultimap.getPreamble().isEmpty();
+
+        // Checks whether both or neither formats are present
+        if (hasUserInputFormat == hasMenuItemFormat) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, MESSAGE_USAGE));
         }
 
+        if (hasUserInputFormat) {
+            return parseUserInputExpense(argMultimap);
+        } else {
+            return parseMenuItemExpense(argMultimap);
+        }
+    }
+
+    /**
+     * Returns true if none of the prefixes contains empty {@code Optional} values in the given
+     * {@code ArgumentMultimap}.
+     */
+    private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
+        return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
+    }
+
+    /**
+     * Returns true if none of the prefixes are repeated
+     * {@code ArgumentMultimap}.
+     */
+    private static boolean hasRepeatedPrefixes(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
+        return !(Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getAllValues(prefix).size() <= 1));
+    }
+
+    /**
+     * Parses the tokenized input according to the "user-input expense" format
+     * @param argMultimap The tokenized input
+     * @return an {@AddExpenseCommand} object for execution
+     * @throws ParseException if the user input does not conform the format
+     */
+    private static AddExpenseCommand parseUserInputExpense(ArgumentMultimap argMultimap) throws ParseException {
         if (hasRepeatedPrefixes(argMultimap, PREFIX_DESCRIPTION, PREFIX_PRICE, PREFIX_CATEGORY, PREFIX_TIMESTAMP)) {
             throw new ParseException(MESSAGE_REPEATED_PREFIX_COMMAND);
         }
@@ -83,19 +118,31 @@ public class AddExpenseCommandParser implements Parser<AddExpenseCommand> {
     }
 
     /**
-     * Returns true if none of the prefixes contains empty {@code Optional} values in the given
-     * {@code ArgumentMultimap}.
+     * Parses the tokenized input according to the "menu item expense" format
+     * @param argMultimap The tokenized input
+     * @return an {@AddExpenseCommand} object for execution
+     * @throws ParseException if the user input does not conform the format
      */
-    private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
-        return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
-    }
+    private static AddExpenseCommand parseMenuItemExpense(ArgumentMultimap argMultimap) throws ParseException {
+        if (hasRepeatedPrefixes(argMultimap, PREFIX_MENU, PREFIX_TIMESTAMP)) {
+            throw new ParseException(MESSAGE_REPEATED_PREFIX_COMMAND);
+        }
 
-    /**
-     * Returns true if none of the prefixes are repeated
-     * {@code ArgumentMultimap}.
-     */
-    private static boolean hasRepeatedPrefixes(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
-        return !(Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getAllValues(prefix).size() <= 1));
-    }
+        MenuItem menuItem = ParserUtil.parseMenuItem(argMultimap.getValue(PREFIX_MENU).get());
+        boolean isTimestampPresent = argMultimap.getValue(PREFIX_TIMESTAMP).isPresent();
 
+        if (isTimestampPresent) {
+            Timestamp timestamp = ParserUtil.parseTimestamp(argMultimap.getValue(PREFIX_TIMESTAMP).get());
+            if (Timekeeper.isFutureTimestamp(timestamp)) {
+                throw new ParseException("Expense cannot be in the future");
+            }
+            Expense expense = new Expense(menuItem, timestamp,
+                    UniqueIdentifierGenerator.generateRandomUniqueIdentifier());
+            return new AddExpenseCommand(expense);
+        } else {
+            Expense expense = new Expense(menuItem,
+                    UniqueIdentifierGenerator.generateRandomUniqueIdentifier());
+            return new AddExpenseCommand(expense);
+        }
+    }
 }
