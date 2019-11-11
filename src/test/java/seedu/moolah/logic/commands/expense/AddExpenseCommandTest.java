067995326becf08eec6a7c78f0efde6fe0af70bb
Ryo Armanda <ryoarmanda@rocketmail.com>
2019-11-11 21:23:36 +0800

    Remove unused import

--- src/test/java/seedu/moolah/logic/commands/expense/AddExpenseCommandTest.java
+++ src/test/java/seedu/moolah/logic/commands/expense/AddExpenseCommandTest.java
@@ -10,7 +10,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
-import java.util.Stack;
 import java.util.function.Predicate;
 
 import org.junit.jupiter.api.Test;
